7248e7256a049450a917ff87c4260be5d54a613c
Iam-VM <44225503+Iam-VM@users.noreply.github.com>
2021-06-02 00:58:30 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,15 @@ import * as React from "react"
 import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
 
+
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import Landing from "../components/landing"
 import About from "../components/about"
 import Guests from "../components/guests"
 import Contact from "../components/contact"
+import TechEvents from "../components/techEvents"
+import NonTechEvents from "../components/nonTechEvents"
 import Pack from "../components/packs"
 import { useEffect, useRef, useState } from "react"
 import Header from "../components/header"
@@ -17,66 +20,51 @@ import Schedule from "../components/schedule"
 import FAQ from "../components/faq"
 
 const IndexPage = () => {
-  const [tabSelected, setTabSelected] = useState("about")
-  const [scrollPosition, setScrollPosition] = useState(0)
-  const aboutRef = useRef(null)
-  const contactRef = useRef(null)
-  const sponsorRef = useRef(null)
-  const aboutBottomRef = useRef(null)
-  import Events from "../components/events"
+  const [tabSelected, setTabSelected] = useState("about");
+  const [scrollPosition, setScrollPosition] = useState(0);
+  const aboutRef = useRef(null);
+  const contactRef = useRef(null);
+  const sponsorRef = useRef(null);
+  const aboutBottomRef = useRef(null);
 
-  const IndexPage = () => (
-    <Layout>
-      <Seo title="Home" />
-      <Landing />
-      <About />
-      <Events />
-    </Layout>
-  )
 
-  const scrollToRef = ref => {
-    window.scrollTo({ top: ref.current.offsetTop, behavior: "smooth" })
+  const scrollToRef = (ref) => {
+    window.scrollTo({top: ref.current.offsetTop, behavior: 'smooth'});
   }
 
   const handleScroll = () => {
-    if (
-      window.pageYOffset <= aboutBottomRef.current.offsetTop &&
-      tabSelected !== "about"
-    ) {
-      setTabSelected("about")
+    if (window.pageYOffset <= aboutBottomRef.current.offsetTop && tabSelected !== "about") {
+      setTabSelected("about");
     }
-  }
+  };
+
 
   useEffect(() => {
-    window.addEventListener("scroll", handleScroll, { passive: true })
+    window.addEventListener('scroll', handleScroll, { passive: true });
 
     return () => {
-      window.removeEventListener("scroll", handleScroll)
-    }
+      window.removeEventListener('scroll', handleScroll);
+    };
   }, [tabSelected])
+
 
   return (
     <Layout>
       <Seo title="Home" />
-      <Header
-        tabSelected={tabSelected}
-        setTabSelected={setTabSelected}
-        scrollToRef={scrollToRef}
-        contactRef={contactRef}
-        sponsorRef={sponsorRef}
-        aboutRef={aboutRef}
-      />
+      <Header tabSelected={tabSelected} setTabSelected={setTabSelected} scrollToRef={scrollToRef} contactRef={contactRef} sponsorRef={sponsorRef} aboutRef={aboutRef} />
       <Landing aboutRef={aboutRef} />
       <About aboutBottomRef={aboutBottomRef} />
       <Quotes />
       <Guests />
       <Pack />
       <Schedule />
-      <Sponsors sponsorRef={sponsorRef} />
+      <Sponsors sponsorRef={sponsorRef}/>
       <Contact contactRef={contactRef} />
       <FAQ />
     </Layout>
-  )
+  );
 }
 
-export default IndexPage
+
+
+export default IndexPage;
