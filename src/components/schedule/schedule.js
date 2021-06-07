a87ff5377f62171bed7e67016c1ae3e7a14eae3d
Iam-VM <vishnumohanantheking@gmail.com>
2021-06-08 00:37:41 +0530

    added schedule

--- src/components/schedule/schedule.js
+++ src/components/schedule/schedule.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react"
 import * as scheduleStyles from "./schedule.module.css";
 
 import scheduleImg from "../../images/timeline.svg"
+import scheduleMBImg from "../../images/mb/schedule.svg"
 import AOS from "aos"
 
 const Schedule = ({scheduleRef, setSideNavBarStatus}) => {
@@ -16,7 +17,12 @@ const Schedule = ({scheduleRef, setSideNavBarStatus}) => {
       <img
         src={scheduleImg}
         alt={"schedule"}
-        className={scheduleStyles.timeLine}
+        className={`${scheduleStyles.timeLine} ${scheduleStyles.desktopVersion}`}
+      />
+      <img
+        src={scheduleMBImg}
+        alt={"schedule"}
+        className={`${scheduleStyles.timeLine} ${scheduleStyles.mobileVersion}`}
       />
     </div>
   );
