0ccfab100d03741164e45cc2df56e27a17e383a6
Iam-VM <vishnumohanantheking@gmail.com>
2021-06-09 21:13:10 +0530

    added epoq visit us link

--- src/components/sponsors/sponsors.js
+++ src/components/sponsors/sponsors.js
@@ -4,7 +4,7 @@ import * as sponsorStyles from "./sponsors.module.css";
 
 import sponsorHeadImg from "../../images/sponsors-head.svg";
 import alumniSponsorImage from "../../images/sponsor-alumni-img.svg";
-import epoqImg from "../../images/epoq.png";
+import epoqImg from "../../images/epoq.svg";
 
 import AOS from "aos"
 
@@ -24,15 +24,26 @@ const Card = ({name, description, imageName, imageClassName}) => {
   );
 }
 
+const CardTitle = ({imageName, description, websiteLink, imageClassName}) => {
+
+  useEffect(() => {
+    AOS.init();
+  }, []);
+
+  return (
+    <div className={sponsorStyles.card}>
+      <img src={imageName} alt={"name"} className={imageClassName} data-aos-once="true" data-aos="zoom-in" data-aos-duration="1500" />
+      <a href={websiteLink} target={"_blank"} className={sponsorStyles.cardTitleLink}>visit us</a>
+    </div>
+  );
+}
+
 const TitleType = () => {
   return (
     <div className={sponsorStyles.typeContainer}>
       <div className={sponsorStyles.typeHead}>Title Sponsor</div>
       <div className={sponsorStyles.cardContainer}>
-        <Card imageClassName={sponsorStyles.epoqImg} imageName={epoqImg}  name={""} description={""} />
-        {/*<Card name={"Full Name"} description={"Description for the sponsor"} />*/}
-        {/*<Card name={"Full Name"} description={"Description for the sponsor"} />*/}
-        {/*<Card name={"Full Name"} description={"Description for the sponsor"} />*/}
+        <CardTitle imageClassName={sponsorStyles.epoqImg} imageName={epoqImg}  websiteLink={"https://epoqzero.com/"} />
       </div>
     </div>
   );
@@ -45,9 +56,6 @@ const FinanceType = () => {
       <div className={sponsorStyles.typeHead}>Financial Sponsor</div>
       <div className={sponsorStyles.cardContainer}>
         <Card imageClassName={sponsorStyles.sponsorImage} imageName={alumniSponsorImage}  name={"Alumni GEC Palakkad"} description={"A token of love and appreciation"} />
-        {/*<Card name={"Full Name"} description={"Description for the sponsor"} />*/}
-        {/*<Card name={"Full Name"} description={"Description for the sponsor"} />*/}
-        {/*<Card name={"Full Name"} description={"Description for the sponsor"} />*/}
       </div>
     </div>
   );
